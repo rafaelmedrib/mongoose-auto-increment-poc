3e3c67e889dde8ac14c20cbc0f875931862d4389
Rafael Medeiros <rafaelmedrib@gmail.com>
2024-02-22 23:22:38 -0300

    add random number of documents for each entity

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ import { AnotherIncrementableEntity } from './entities/anotherIncrementableEntit
   try {
     await mongoose.connect("mongodb://localhost:27017/test");
 
-    for(let i = 0; i < 5; i++) {
+    const random_integer = (maximum = 1000) => Math.floor(Math.random() * maximum);
+
+    for(let i = 0; i < random_integer(); i++) {
     const entity = new IncrementableEntity({ name: `test_name_${i}` });
     await entity.save();
+    } 
 
-    const another_entity = new AnotherIncrementableEntity({ description: `test_description_${i}` });
-    await another_entity.save();
+    for(let i = 0; i < random_integer(); i++) {
+      const another_entity = new AnotherIncrementableEntity({ description: `test_description_${i}` });
+      await another_entity.save();
     }
   } catch (error) {
     console.log("🚀 ~ ; ~ error:", error)
